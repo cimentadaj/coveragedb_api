4a5480728b647c1dc67f7d1fcad52d23443c87f0
Jorge Cimentada <cimentadaj@gmail.com>
2021-10-30 19:20:11 +0200

    Adds POSTGRES deployment keys and docker image

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,22 @@
+from databases import DatabaseURL
 from starlette.config import Config
+from starlette.datastructures import Secret
 
 config = Config("./.env")
 
 PROJECT_NAME = config("PROJECT_NAME", cast=str)
 PROJECT_VERSION = config("PROJECT_VERSION", cast=str)
+
+SECRET_KEY = config("SECRET_KEY", cast=Secret)
+POSTGRES_USER = config("POSTGRES_USER", cast=str)
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=str)
+POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str)
+POSTGRES_PORT = config("POSTGRES_PORT", cast=str)
+POSTGRES_DB = config("POSTGRES_DB", cast=str)
+
+DATABASE_URL = config(
+    "DATABASE_URL",
+    cast=DatabaseURL,
+    default=
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
